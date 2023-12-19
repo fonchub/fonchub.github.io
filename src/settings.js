7c92a553e7830d2bb685ec10a9ab15c701720872
zhb <1013045524@qq.com>
2023-12-19 14:25:47 +0800

    更换游戏

--- src/settings.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/GameLoad.fire",orientation:"portrait",jsList:["assets/Script/common/Config.js"]};
